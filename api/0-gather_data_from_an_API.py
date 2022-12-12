718d0cb33882bafef82cef86bb8aff1ee1c3d15f
Jehp00 <Jehp01104006@gmail.com>
2022-12-12 18:42:22 -0500

    check

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -10,5 +10,6 @@ url = 'https://jsonplaceholder.typicode.com/todos/'
 response = requests.get(url)
 response_json = response.json()
 return("Employee {} is done with tasks\
-    ({}/{}):".format(response_json.name, 
-    NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+    ({}/{}):".format(response_json.EMPLOYEE_NAME,
+                     response_json.NUMBER_OF_DONE_TASKS,
+                     response_json.TOTAL_NUMBER_OF_TASKS))
