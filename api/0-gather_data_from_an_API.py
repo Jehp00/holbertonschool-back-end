333dd9775a79d72c4dba5fc64f6b8e1403f020f4
Jehp00 <Jehp01104006@gmail.com>
2022-12-12 19:48:27 -0500

    check

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,17 +3,39 @@
 Script than return the Todo list with
     name fo employee
     number of done tasks
-    total number of tasks
-    task title
+    number of tasks
 """
 
-import json
 import requests
+import sys
 
-url = 'https://jsonplaceholder.typicode.com/todos/'
-response = requests.get(url)
-response_json = response.json()
-print("Employee {} is done with tasks ({}/{}):".
-                format(response_json.EMPLOYEE_NAME,
-                       response_json.NUMBER_OF_DONE_TASKS,
-                       response_json.TOTAL_NUMBER_OF_TASKS))
+url = 'https://jsonplaceholder.typicode.com/todos?userId='
+url_name = 'https://jsonplaceholder.typicode.com/users?id='
+
+
+def get_param():
+    """substract elemnts from the api"""
+    response = requests.get(url + sys.argv[1])
+    response_name = requests.get(url_name + sys.argv[1])
+
+    content = [response.json()]
+    content_name = [response.json()]
+
+    completed_tasks = 0
+    num_of_tasks = 0
+    tasks = []
+
+    for t in content:
+        if t['completed']:
+            completed_tasks += 1
+            completed_tasks.append(t['title'])
+        tasks += 1
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        content_name[0]['name'], task_completed, total_tasks))
+    for t in completed_tasks:
+        print('\t' + t)
+
+
+if __name__ == '__main__':
+    get_param()
