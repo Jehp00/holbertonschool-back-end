53cf270131a1bd2c4257b0caffc95f2d6aa03205
Jehp00 <Jehp01104006@gmail.com>
2022-12-13 09:56:56 -0500

    task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,38 +1,43 @@
 #!/usr/bin/python3
 """
-This module containts an api request
+Script than return the Todo list with
+    name fo employee
+    number of done tasks
+    number of tasks
 """
 import requests
 import sys
 
 
-def gather_data_from_api():
-    """
-        This function gather data from an api
-    """
+url = 'https://jsonplaceholder.typicode.com/todos?userId='
+url_name = 'https://jsonplaceholder.typicode.com/users?id='
 
-    url_todo = 'https://jsonplaceholder.typicode.com/todos?userId='
-    url_name = 'https://jsonplaceholder.typicode.com/users?id='
-    response_todo = requests.get(url_todo + sys.argv[1])
+
+def gather_data_from_an_api():
+    """substract elemnts from the api"""
+
+    response = requests.get(url + sys.argv[1])
     response_name = requests.get(url_name + sys.argv[1])
 
-    content_todo = list(response_todo.json())
+    content = list(response.json())
     content_name = list(response_name.json())
 
-    task_completed = 0
-    total_tasks = 0
-    completed_tasks = []
-    for elem in content_todo:
-        if elem['completed']:
-            task_completed += 1
-            completed_tasks.append(elem['title'])
-        total_tasks += 1
+    completed_tasks = 0
+    num_of_tasks = 0
+    tasks = []
+
+    for t in content:
+        if t['completed']:
+            completed_tasks += 1
+            tasks.append(t['title'])
+        num_of_tasks += 1
 
     print("Employee {} is done with tasks({}/{}):".format(
-        content_name[0]['name'], task_completed, total_tasks))
-    for elem in completed_tasks:
-        print('\t ' + elem)
+        content_name[0]['name'], completed_tasks, num_of_tasks))
+
+    for t in tasks:
+        print('\t ' + t)
 
 
 if __name__ == "__main__":
-    gather_data_from_api()
+    gather_data_from_an_api()
